71bcc393df7f3b01ed93a4783302d6b69b096d68
junshandian <junshandian@qq.com>
2024-08-04 19:53:54 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, Git");
+        System.out.println("Hello, Git2");
     }
 }
