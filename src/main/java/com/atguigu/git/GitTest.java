38b2ff034c09d590b978886a60da60e2ef3f5676
junshandian <junshandian@qq.com>
2024-08-04 20:02:14 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello, Git");
         System.out.println("Hello, Git2");
         System.out.println("Hello, Git3");
+        System.out.println("Hello, Git4");
     }
 }
