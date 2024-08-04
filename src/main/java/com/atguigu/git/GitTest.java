c4f95c394c5fc5bc188dd6311ca6330ee1b1c30e
junshandian <junshandian@qq.com>
2024-08-04 20:06:08 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello, Git2");
         System.out.println("Hello, Git3");
         System.out.println("Hello, Git4");
+        System.out.println("master test");
     }
 }
